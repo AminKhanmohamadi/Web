abf8918ddaddd131aeb233c9c8339b0f47db29dd
amin <aminkhm097@gmail.com>
2025-02-11 21:02:41 +0330

    feat: init serializers

--- src/serializers.py
+++ src/serializers.py
@@ -7,15 +7,21 @@ from .models import Knowledge , Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id' , 'title' , 'created_by' , 'created_at' , 'updated_at' , 'summery']
+        fields = ["id", "title", "description", "created_by", "created_at", "updated_at", "summery", "status"]
 
 
 class KnowledgeSerializer(serializers.ModelSerializer):
-    article = serializers.HyperlinkedRelatedField(view_name='article-detail', read_only=True , many=True)
+    articles = serializers.SerializerMethodField()
     class Meta:
         model = Knowledge
-        fields = ['id' , 'title' , 'created_by' , 'created_at' , 'updated_at' , 'article']
+        fields = ["id", "title", "created_by", "created_at", "updated_at", "status", "articles"]
 
+    def get_articles(self, obj):
+        user = self.context["request"].user
+        articles = obj.article.all()
 
+        if not user.is_superuser:
+            articles = articles.filter(status="Pub")
+        return ArticleSerializer(articles, many=True, context=self.context).data
 
 
